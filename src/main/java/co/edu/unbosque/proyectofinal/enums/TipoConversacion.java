a7c7026eb5244387f6f8e107911c1ca2cabc546d
0hsamm <samuelop3308@gmail.com>
2026-05-15 14:57:27 -0500

    Revert "Primer adelanto del proyecto"
    This reverts commit c67e2c981e21822bcce35f03c4365a481f3ea4b0.

--- src/main/java/co/edu/unbosque/proyectofinal/enums/TipoConversacion.java
+++
@@ -1,5 +0,0 @@
-package co.edu.unbosque.proyectofinal.enums;
-
-public enum TipoConversacion {
-	DIRECT
-}
--- src/main/java/co/edu/unbosque/proyectofinal/enums/TipoMensaje.java
+++
@@ -1,7 +0,0 @@
-package co.edu.unbosque.proyectofinal.enums;
-
-public enum TipoMensaje {
-	TEXTO,
-	AUDIO,
-	IA
-}
